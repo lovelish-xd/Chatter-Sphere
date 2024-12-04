3ebbd709eed2313dc00ee5d21a4878c7d86510a3
Lovelish Chauhan <83005550+lovelish-xd@users.noreply.github.com>
2024-12-04 21:50:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://chatter-sphere-gules.vercel.app/", // Your React app's origin
+    origin: https://chatter-sphere-frontend-4titgdb9h-lovelish-xds-projects.vercel.app/", // Your React app's origin
     methods: ["GET", "POST"],
     credentials: true,
   },
